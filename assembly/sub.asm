569a3ee0da5fd4039018c24be7f25638e0f0f205
Otamio <ca.john.wang@gmail.com>
2019-03-07 02:22:17 -0600

    mult

--- assembly/sub.asm
+++ assembly/sub.asm
@@ -1,55 +1,8 @@
-print_big:
-# save state
-  addi $sp, $sp, -36          # 8 elements are pushed onto the stack
-  sw $s0, 32($sp)
-  sw $s1, 28($sp)
-  sw $s2, 24($sp)
-  sw $s3, 20($sp)
-  sw $s4, 16($sp)
-  sw $s5, 12($sp)
-  sw $s6, 8($sp)
-  sw $s7, 4($sp)
-  sw $ra, 0($sp)
 
-# read parameters
-  move $s0, $a0               # $s0 is the starting address of b
 
-# loop init
-  lw $s7, 0($s0)
-  addi $s7, $s7, -1           # $s7 = c (b.n-1)
-  addi $s6, $s0, 4            # $s6 = &b.digits[]
-  blt $s7, $0, PG_exit        # branch if c<0
 
-# loop
-BG_loop:
-  move $t0, $s7               # $t0 = c
-  sll $t0, $t0, 2             # $t0 = 4*$s7 = 4c
-  add $t0, $s6, $t0           # $t0 = &b.digits[c]
-  lw $a0, 0($t0)              # load b.digits[c]
-  li $v0, 1                   # load print sys call
-  syscall                     # print element
-  addi $s7, $s7, -1           # $s7 = $s7 - 1
-  blt $s7, $0, PG_exit        # branch if c<0
-  j BG_loop
 
-# end of loop (print newline)
-PG_exit:
-  la $a0, newline             # load linefeed into $a0 for printing
-  li $v0, 4                   # load print string syscall code
-  syscall
 
-PG_return:
-# recover state
-  lw $s0, 32($sp)
-  lw $s1, 28($sp)
-  lw $s2, 24($sp)
-  lw $s3, 20($sp)
-  lw $s4, 16($sp)
-  lw $s5, 12($sp)
-  lw $s6, 8($sp)
-  lw $s7, 4($sp)
-  lw $ra, 0($sp)
-  addi $sp, $sp, 36           # 8 elements are popped from the stack
 
-# return
-  jr $ra
+# print output
+  jal print_big
